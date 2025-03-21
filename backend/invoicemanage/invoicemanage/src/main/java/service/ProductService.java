f0ef0d052e0ebb88907bad58be7418c64bf3595b
Hoai Thu <nguyenthihoaithu014@gmail.com>
2025-03-21 10:00:03 +0700

    cau truc

---
+++ backend/invoicemanage/invoicemanage/src/main/java/service/ProductService.java
@@ -0,0 +1,18 @@
+package service;
+
+import entity.Product;
+import repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
