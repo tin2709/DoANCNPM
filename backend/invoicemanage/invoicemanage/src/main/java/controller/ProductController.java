d44af965c7cf1ecf732cb1f69fea76e2997db8c7
thu553 <21130553@st.hcmuaf.edu.vn>
2025-03-21 10:31:27 +0700

    cau truc

--- backend/invoicemanage/invoicemanage/src/main/java/controller/ProductController.java
+++ backend/invoicemanage/invoicemanage/src/main/java/controller/ProductController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import entity.Product;
+import service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getAllProducts();
+    }
 }
